8df03c64798a9684fb92ada3b91b05cc1b86b8ae
omigusty <omistyfani@gmail.com>
2022-12-25 19:58:06 +0700

    create download feature

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
 app = Flask(__name__)
+
+datas = []
 
 
 @app.route("/")
 def index():
     try:
-        datas = []
         for pages in range(1, 11):
             urlTarget = requests.get(
                 "https://quotes.toscrape.com/page/"+str(pages))
@@ -21,9 +23,16 @@ def index():
 
                 datas.append({"quote": getQuote, "author": getAuthor})
 
+            # saveFile = pd.DataFrame(datas)
+            # saveFile.to_csv("data_quote.csv", encoding="utf-8")
+
         return render_template("index.html", datas=datas)
     except Exception as err:
         print("error", err)
+
+
+def download():
+    index.data
 
 
 if __name__ == "__main__":
